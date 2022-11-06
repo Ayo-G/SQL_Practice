010bbcf50b402bb456be48547b28a9edf7f92da3
Ayomide Gbadegesin <110608447+Ayo-G@users.noreply.github.com>
2022-11-06 16:08:53 +0100

    Create counting_instances_in_text.sql

---
+++ Google/Counting Instances in Text/counting_instances_in_text.sql
@@ -0,0 +1,30 @@
+``` sql
+
+with bull as
+    (select
+        'bull' as text,
+        count(contents) as occurence
+    from google_file_store
+    where 
+        contents like ' bull %' 
+        or contents like '% bull %'),
+bear as 
+    (select
+        'bear' as text,
+        count(contents) as occurence
+    from google_file_store
+    where 
+        contents like ' bear %' 
+        or contents like '% bear %')
+
+select 
+    *
+from
+    bull
+union
+select
+    *
+from
+    bear;
+
+```
