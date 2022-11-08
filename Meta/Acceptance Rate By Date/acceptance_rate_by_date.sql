ce63f5783ee13085ffe413c7602381802816862f
Ayomide Gbadegesin <110608447+Ayo-G@users.noreply.github.com>
2022-11-08 17:14:57 +0100

    Create acceptance_rate_by_date.sql

---
+++ Meta/Acceptance Rate By Date/acceptance_rate_by_date.sql
@@ -0,0 +1,35 @@
+``` sql
+
+WITH sent as
+     (SELECT 
+         date,
+				 user_id_receiver,
+		     user_id_sender
+      FROM 
+         fb_friend_requests
+      WHERE
+         action = 'sent'),
+accepted as
+    (SELECT
+        date, 
+		user_id_receiver,
+		user_id_sender
+    FROM
+        fb_friend_requests
+    WHERE
+        action = 'accepted')
+SELECT 
+    s.date,
+    COUNT(a.user_id_receiver)/cast(COUNT(s.user_id_sender) as decimal) as acceptance_rate
+FROM 
+    sent s
+LEFT JOIN
+    accepted a
+ON
+    s.user_id_receiver = a.user_id_receiver
+AND  
+	s.user_id_sender = a.user_id_sender
+GROUP BY
+    s.date
+
+```
