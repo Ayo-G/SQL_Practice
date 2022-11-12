9e57ca44a55551b70df3d4b3826b6b32406419e4
Ayomide Gbadegesin <110608447+Ayo-G@users.noreply.github.com>
2022-11-12 14:48:31 +0100

    Create highest_cost_orders.sql

---
+++ Amazon/Highest Cost Orders/highest_cost_orders.sql
@@ -0,0 +1,30 @@
+``` sql
+
+with cte as
+    (select 
+        first_name,
+        sum(total_order_cost) as total,
+        dense_rank() over(order by sum(total_order_cost) desc) as rank_,
+        order_date
+    from 
+        orders o
+    left join
+        customers c
+    on 
+        o.cust_id = c.id
+    group by
+        first_name,
+        order_date)
+
+select
+    first_name,
+    total,
+    order_date
+from
+    cte
+where
+    rank_ = 1
+    and
+    order_date between '2019-02-01' and '2019-05-01'
+
+```
