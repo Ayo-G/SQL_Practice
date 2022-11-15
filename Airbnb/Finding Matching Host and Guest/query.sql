c2b2cdc0808396a5cf4fad227df27aeea72fa2f3
Ayomide Gbadegesin <110608447+Ayo-G@users.noreply.github.com>
2022-11-15 19:23:31 +0100

    Create query.sql

---
+++ Airbnb/Finding Matching Host and Guest/query.sql
@@ -0,0 +1,16 @@
+``` sql
+
+select 
+    distinct
+    host_id,
+    guest_id
+from
+    airbnb_hosts ah
+left join
+    airbnb_guests ag
+on
+    ah.nationality = ag.nationality
+and
+    ah.gender = ag.gender
+
+```
